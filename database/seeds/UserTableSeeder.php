89f51f236184b2ab721cdfc3bbc98d0dbc53e0af
Michele Ancheta <michelee.ancheta@gmail.com>
2015-05-04 23:34:51 -0430

    Creacion  de seeders

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,15 +1,33 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class UserTableSeeder extends Seeder {
 
     public function run(){
-        \DB::table('users')->insert(array(
-            'name' => 'Michele',
-            'email' => 'm@gmail.com',
-            'password' => \Hash::make('secreto')
-        ));
+
+        $faker = Faker::create();
+
+        for ($i=0; $i<30; $i++ ){
+
+            $id = \DB::table('users')->insert(array(
+                'first_name' => $faker->firstName,
+                'last_name'  => $faker->lastName,
+                'email'      => $faker->unique()->email,
+                'password'   => \Hash::make('123456'),
+                'type'       => 'user'
+            ));
+
+            \DB::table('users_profiles')->insert(array(
+                'user_id'   => $id,
+                'bio'       => $faker->paragraph(rand(2,5)),
+                'website'   => 'http://www.'.$faker->domainName,
+                'twitter'   => 'http://www.twitter.com/'.$faker->userName
+
+            ));
+        }
+
     }
 
 
